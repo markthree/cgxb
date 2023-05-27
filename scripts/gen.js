b475bdfe506a4e3af42ac92a568639ed9e5ffb0f
markthree <1801982702@qq.com>
2023-05-27 16:59:56 +0800

    feat: 支持命令行自动创建文章

---
+++ scripts/gen.js
@@ -0,0 +1,35 @@
+import { existsSync } from "fs";
+import { writeFile } from "fs/promises";
+import { join, resolve } from "path";
+
+async function gen() {
+  const now = new Date().toLocaleDateString();
+
+  const filePath = join("docs", `${now}.md`);
+
+  // 如果已经存在则跳出
+  if (existsSync(filePath)) {
+    return;
+  }
+
+  await writeFile(
+    filePath,
+    `---
+title: ${now.replace(/\//g, '-')}
+---
+
+# {{ $frontmatter.title }}
+
+## 仓库快读
+
+> 
+
+<br />
+<br />
+
+## 文章小报
+`,
+  );
+}
+
+gen();
